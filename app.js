e806c22a7acae22f337dc6d497c69790199dc871
Ebert Rodrigues <ebertcrn@gmail.com>
2024-03-15 12:29:39 -0300

    section 4

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input name="messageInserted" type="text"><button type="submit">Submit</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            // console.log(`chunk: `, chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            // console.log(`parsedBody: `, parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                // console.log(err);
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My Title</title></head>');
-    res.write('<body><h1>Hello from my NodeJS server</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
